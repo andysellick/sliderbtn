f45517b6361fcf143201a5b98219849e640e11cf
andysellick <andy.sellick@tangentsnowball.com>
2015-03-18 17:10:11 +0000

    Improving responsiveness and adding options

--- sliderbtn.jquery.js
+++ sliderbtn.jquery.js
@@ -7,6 +7,9 @@
 
     Options:
         'numbering': 0 or 1, causes indexing of elements to either start from 0 or 1, defaults to 1
+        'initial': -1 off, 0 or higher sets which link to initialise as the active one on load
+        'initialInstant': sets whether initialisation of hilight should be instant or animated
+        'speed': sets speed of animation, ms
 
     Callback:
         $('.sliderbtn').sliderbtn({
@@ -15,7 +18,7 @@
                 console.log('Item clicked was number ' + num + ', ' + item);
             }
         });
-        
+
         Callback has two parameters:
             - num is the index of the element clicked on
             - item is the value of the data-item attribute of the element clicked on, if it exists
@@ -35,41 +38,61 @@
 
 			this.settings = $.extend({
 				numbering: 1, //given a non-zero value, numbers the elements from 1 to x rather than 0 to x
+				initial: -1, //sets which link to initialise as the active one, if 0 or greater
+				initialInstant: 1, //sets whether initialisation of hilight should be instant or animated
+				speed: 500, //sets speed of animation, ms
 				onClickChange: function() {}
 			}, this.defaults, this.options);
 
 			var clinks = this.$elem.find('a');
-			//create the slider
-			var $hilight = $('<div/>').addClass('hilight').css('width',clinks.first().outerWidth()).appendTo(this.$elem);
-			var numbering = this.settings.numbering;
-			var onClickChange = this.settings.onClickChange;
+			var $hilight = $('<div/>').addClass('hilight').css('width',clinks.first().outerWidth()).appendTo(this.$elem); //create the slider
+            if(thisobj.settings.initial > clinks.length - 1){
+                thisobj.settings.initial = clinks.length - 1;
+            }
+
+			if(thisobj.settings.initial >= 0){
+                animateSlider(this.$elem.find('a:eq(' + thisobj.settings.initial + ')'),thisobj.settings.initialInstant);
+            }
 
 			clinks.on('click',function(e){
                 e.preventDefault();
+                animateSlider($(this));
+
+                //get variables for callback
+                var num = $(this).index();
+                if(thisobj.settings.numbering)
+                    num += 1;
+                var item = $(this).data('item') || "";
+    			thisobj.settings.onClickChange.call(this,num,item); //call callback
+            });
+
+            function animateSlider(thisel,instant=0){
                 clinks.removeClass('active');
-                $(this).addClass('active');
+                thisel.addClass('active');
 
-                var p = $(this).position();
-                var w = $(this).outerWidth(true);
-                var h = $(this).outerHeight(true);
+                var p = thisel.position();
+                var w = thisel.outerWidth(true);
+                var h = thisel.outerHeight(true);
 
+                var speed = thisobj.settings.speed;
+                if(instant){
+                    console.log('instant');
+                    speed = 0;
+                }
                 $hilight.animate({
                     'left': p.left,
                     'top': p.top,
                     'width': w,
                     'height': h
-                });
+                },speed);
+            }
 
-                //get variables for callback
-                var num = $(this).index();
-                if(numbering)
-                    num += 1;
-                var item = $(this).data('item') || "";
-
-                //call callback
-    			onClickChange.call(this,num,item);
+        	$(window).on('resize',function(){
+                var $curr = thisobj.$elem.find('.active');
+                if($curr){
+                    animateSlider($curr,1);
+                }
             });
-        	/* $(window).on('resize',function(){}); */
 		},
 
 		//below a given screen size, remove all plugin functionality
